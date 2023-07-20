c10076b8a5641e6c0ab017c6b8be2fde8d38a06a
Bekzod <mirzaahmedov.dev@gmail.com>
2023-07-20 13:02:37 +0500

    lazy neovim config files

---
+++ .config/nvim/lua/core/plugins.lua
@@ -0,0 +1,103 @@
+return {
+-- VimBeGood
+'ThePrimeagen/vim-be-good',
+
+-- ColorBuddy
+-- 'tjdevries/colorbuddy.nvim',
+-- 'svrana/neosolarized.nvim',
+-- Packer can manage itself
+-- 'wbthomason/packer.nvim',
+-- CSS colors
+-- 'ap/vim-css-color',
+
+-- Rose Pine color scheme
+{ 'rose-pine/neovim', name = 'rose-pine' },
+
+-- Github copilot
+'github/copilot.vim',
+
+-- NightFox color scheme
+-- 'EdenEast/nightfox.nvim',
+-- Sonokai color scheme
+-- 'sainnhe/sonokai',
+-- Edge color scheme
+-- 'sainnhe/edge',
+--   -- Darcula color scheme
+-- 'doums/darcula',
+--   -- Tokyodark color scheme
+-- 'tiagovla/tokyodark.nvim',
+--   -- Vscode color scheme
+-- 'Mofiqul/vscode.nvim',
+--   -- One dark color scheme
+-- 'navarasu/onedark.nvim',
+--   -- Nebulous color scheme
+-- 'Yagua/nebulous.nvim',
+
+-- Comments
+{
+  'numToStr/Comment.nvim',
+  config = function()
+    require('Comment').setup()
+  end
+},
+-- 'JoosepAlviste/nvim-ts-context-commentstring',
+'mxw/vim-jsx',
+
+-- Nerd Tree
+'preservim/nerdtree',
+'johnstef99/vim-nerdtree-syntax-highlight',
+'ryanoasis/vim-devicons',
+
+-- Post-install/update hook with neovim command
+{ 'nvim-treesitter/nvim-treesitter', build = ':TSUpdate' },
+
+-- LSP setup
+{
+    'VonHeikemen/lsp-zero.nvim',
+    branch = 'v1.x',
+    dependencies = {
+      -- LSP Support
+      { 'neovim/nvim-lspconfig' },           -- Required
+      { 'williamboman/mason.nvim' },         -- Optional
+      { 'williamboman/mason-lspconfig.nvim' }, -- Optional
+
+      -- Autocompletion
+      { 'hrsh7th/nvim-cmp' },       -- Required
+      { 'hrsh7th/cmp-nvim-lsp' },   -- Required
+      { 'hrsh7th/cmp-buffer' },     -- Optional
+      { 'hrsh7th/cmp-path' },       -- Optional
+      { 'saadparwaiz1/cmp_luasnip' }, -- Optional
+      { 'hrsh7th/cmp-nvim-lua' },   -- Optional
+
+      -- Snippets
+      { 'L3MON4D3/LuaSnip' },           -- Required
+      { 'rafamadriz/friendly-snippets' }, -- Optional
+    }
+  },
+
+{
+  "windwp/nvim-autopairs",
+  config = function() require("nvim-autopairs").setup {} end
+},
+
+{
+  'nvim-telescope/telescope.nvim', tag = '0.1.1',
+  -- or                          , branch = '0.1.x',
+  dependencies = { { 'nvim-lua/plenary.nvim' } }
+},
+
+-- split windows
+-- 'christoomey/vim-tmux-navigator',
+-- 'szw/vim-maximizer',
+'tpope/vim-surround',
+
+-- status line
+'vim-airline/vim-airline',
+'vim-airline/vim-airline-themes',
+
+-- prettier extension
+'sbdchd/neoformat',
+
+-- dev icons
+-- 'nvim-tree/nvim-web-devicons',
+}
