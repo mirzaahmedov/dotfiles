841c0184cf8c04a072433790d236aaac6836aeab
Bekzod <mirzaahmedov.dev@gmail.com>
2023-10-06 12:13:15 +0500

    commit

--- .scripts/set-wallpaper.sh
+++ .scripts/set-wallpaper.sh
@@ -4,7 +4,7 @@ config_file=$HOME/.system/config.json
 image_path=$1
 
 if [ $image_path ]; then
-  jq ".wallpaper = \"$image_path\"" $config_file > $config_file
+  echo $(jq ".wallpaper = \"$image_path\"" $config_file) > $config_file
   swaymsg "output * bg $image_path fill"
 else
   image_path=$(jq ".wallpaper" $config_file)
